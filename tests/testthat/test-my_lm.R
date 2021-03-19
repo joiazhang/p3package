1c735a63266cde23375e9f0dacf47303a81c19ea
joiazhang <joiaz@uw.edu>
2021-03-19 05:46:08 -0700

    u

--- tests/testthat/test-my_lm.R
+++ tests/testthat/test-my_lm.R
@@ -1,3 +1,5 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+output <- my_lm(lifeExp ~ gdpPercap + continent, data = my_gapminder)
+test_that("output correct", {
+  expect_equal(my_lm(lifeExp ~ gdpPercap + continent, data = my_gapminder), output)
 })
