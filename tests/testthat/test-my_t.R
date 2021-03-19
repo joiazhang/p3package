c56ab7bf579e66864dbbafc2441b727e3b3d6d6c
joiazhang <joiaz@uw.edu>
2021-03-19 05:08:03 -0700

    d

--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -8,7 +8,7 @@ test_that("alternative = greater works", {
 
 test_that("alternative = two.sided works", {
   expect_equal(my_t.test(my_gapminder$lifeExp, alternative = "two.sided", 60), list("test_stat" = -1.67954774, "df" = 1703, "alternative" = "two.sided", "p_val" = 0.09322877))
-  expect_equal(my_t.test(x = my_gapminder$lifeExp, mu = 60), list("test_stat" = -1.6795477, "df" = 1703, "alternative" = "two.sided", "p_val" = 0.09322877))
+  expect_equal(my_t.test(x = my_gapminder$lifeExp, mu = 60), list("test_stat" = -1.67954774, "df" = 1703, "alternative" = "two.sided", "p_val" = 0.09322877))
 })
 
 test_that("invalid alternative input throws error", {
