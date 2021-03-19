e0ef04a1eace78cc9590636503000171147bc66b
joiazhang <joiaz@uw.edu>
2021-03-19 05:04:43 -0700

    d

--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -1,13 +1,13 @@
 test_that("alternative = less works", {
-  expect_equal(my_t.test(my_gapminder$lifeExp, alternative = "less", 60), list("test_stat" = -1.679548, "df" = 1703, "alternative" = "less", "p_val" = 0.04661438))
+  expect_equal(my_t.test(my_gapminder$lifeExp, alternative = "less", 60), list("test_stat" = -1.6795477, "df" = 1703, "alternative" = "less", "p_val" = 0.046614385))
 })
 
 test_that("alternative = greater works", {
-  expect_equal(my_t.test(my_gapminder$lifeExp, alternative = "greater", 60), list("test_stat" = -1.679548, "df" = 1703, "alternative" = "greater", "p_val" = 0.9533856))
+  expect_equal(my_t.test(my_gapminder$lifeExp, alternative = "greater", 60), list("test_stat" = -1.6795477, "df" = 1703, "alternative" = "greater", "p_val" = 0.95338562))
 })
 
 test_that("alternative = two.sided works", {
-  expect_equal(my_t.test(my_gapminder$lifeExp, alternative = "two.sided", 60), list("test_stat" = -1.679548, "df" = 1703, "alternative" = "two.sided", "p_val" = 0.09322877))
+  expect_equal(my_t.test(my_gapminder$lifeExp, alternative = "two.sided", 60), list("test_stat" = -1.6795477, "df" = 1703, "alternative" = "two.sided", "p_val" = 0.09322877))
   expect_equal(my_t.test(x = my_gapminder$lifeExp, mu = 60), list("test_stat" = -1.679548, "df" = 1703, "alternative" = "two.sided", "p_val" = 0.09322877))
 })
 
